09af661ea2002236e4ea2320fe29ec22b1f694e6
bolita <bolita@bolita-laptop.(none)>
2014-08-19 23:10:53 -0500

    For R ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+      
+      ## Initialize inverse of X
+      invx <- NULL
+      ## Set the matrix
+      set <- function( m ) {
+            x <<- m
+            invx <<- NULL
+      }
+      ## Get the matrix
+      get <- function() {
+            ## Return matrix
+            x
+      }
+      ## Set the inverse of the matrix
+      setInverse <- function(inverse) {
+            invx <<- inverse
+      }
+      ## Get the inverse of the matrix
+      getInverse <- function() {
+            ## Inverse x
+            invx
+      }
+      ## Return list of the methods
+      list(set = set, get = get, 
+           setInverse = setInverse, 
+           getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve 
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+      ## Matrix that is the inverse of x
+      invx <- x$getInverse()
+      ## Return inverse if its already set
+      if( !is.null(invx) ) {
+            message("getting cached data")
+            return(invx)
+      }else{
+            ## Get matrix 
+            data <- x$get()
+            ## Calculate inverse 
+            invx <- solve(data) %*% data
+            ## Set the inverse 
+            x$setInverse(invx)
+            ## Return the matrix
+            return(invx)
+      }
 }
+
